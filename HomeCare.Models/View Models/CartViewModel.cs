1c2af0b327e17f21439bfc2610db5ceecb8d968f
Dennis <dmarlo96@live.ca>
2020-12-19 22:44:48 -0500

    Shopping Cart and Order Functionality

---
+++ HomeCare.Models/View Models/CartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCare.Models.View_Models
+{
+    public class CartViewModel
+    {
+        public IList<Service> ServiceList { get; set;  }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
--- HomeCare.Utility/SD.cs
+++ HomeCare.Utility/SD.cs
@@ -7,5 +7,9 @@ namespace HomeCare.Utility
     public static class SD
     {
         public const string SessionCart = "Cart";
+
+        public const string StatusSubmitted = "Submitted";
+        public const string StatusApproved = "Approved";
+        public const string StatusRejected = "Rejected";
     }
 }
